08ff4a27c76d5c7ef3451ac2cc0c84d18674a6cc
CubeBeveled <cubebeveled@gmail.com>
2026-01-28 13:48:09 +0100

    idk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 const { Command } = require("commander");
 const path = require("path");
+const fs = require("fs");
 
 const { version } = require("./package.json");
 const program = new Command();
@@ -23,50 +24,87 @@ program
   )
   .argument("<dump folder>", "The folder where the dumps will be stored")
   .argument("[dump file name]", "The name of the dump file")
+  .option("--repeat <delay>", "Keep saving dumps")
   .action(
     (host, port, database, username, password, dumpFolder, dumpFileName) => {
-      let testCmd;
+      saveDump(
+        host,
+        port,
+        database,
+        username,
+        password,
+        dumpFolder,
+        dumpFileName,
+      );
 
-      if (process.platform === "win32") {
-        testCmd = "where mysqldump";
-      } else {
-        testCmd = "which mysqldump";
-      }
-
-      exec(testCmd, (error, stdout, stderr) => {
-        if (error || !stdout.trim()) {
-          program.error("mysqldump not found");
-        } else {
-          if (!dumpFileName) {
-            const now = new Date();
-
-            const year = now.getFullYear();
-            const month = String(now.getMonth() + 1);
-            const day = String(now.getDate());
-            const hours = String(now.getHours());
-            const minutes = String(now.getMinutes());
-            const seconds = String(now.getSeconds());
-
-            dumpFileName = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
-          }
-
-          if (!dumpFileName.includes(".")) dumpFileName += ".sql";
-
-          let dumpPath = path.join(dumpFolder, dumpFileName);
-
-          exec(
-            `mysqldump -h "${host}" -P ${port} --password="${password}" -u "${username}" "${database}" > "${dumpPath}"`,
-            (error, stdout, stderr) => {
-              if (error) {
-                program.error(`Dump error: ${error.message}`);
-              }
-
-              console.log("Dump saved to", dumpPath);
-            },
+      const opts = program.opts();
+      if (opts.repeat)
+        setInterval(() => {
+          saveDump(
+            host,
+            port,
+            database,
+            username,
+            password,
+            dumpFolder,
+            dumpFileName,
           );
-        }
-      });
+        });
     },
   );
 
 program.parse();
+
+function saveDump(
+  host,
+  port,
+  database,
+  username,
+  password,
+  dumpFolder,
+  dumpFileName,
+) {
+  let testCmd;
+
+  if (process.platform === "win32") {
+    testCmd = "where mysqldump";
+  } else {
+    testCmd = "which mysqldump";
+  }
+
+  exec(testCmd, (error, stdout, stderr) => {
+    if (error || !stdout.trim()) {
+      program.error("mysqldump not found");
+    } else {
+      if (!dumpFileName) {
+        const now = new Date();
+
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1);
+        const day = String(now.getDate());
+        const hours = String(now.getHours());
+        const minutes = String(now.getMinutes());
+        const seconds = String(now.getSeconds());
+
+        dumpFileName = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+      }
+
+      if (!dumpFileName.includes(".")) dumpFileName += ".sql";
+
+      let dumpPath = path.join(dumpFolder, dumpFileName);
+
+      if (fs.existsSync(dumpPath)) fs.rmSync(dumpPath);
+
+      exec(
+        `mysqldump -h "${host}" -P ${port} --password="${password}" -u "${username}" "${database}" > "${dumpPath}"`,
+        (error, stdout, stderr) => {
+          if (error) {
+            program.error(`Dump error: ${error.message}`);
+          }
+
+          console.log("Dump saved to", dumpPath);
+        },
+      );
+    }
+  });
+}
